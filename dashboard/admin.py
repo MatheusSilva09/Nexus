0d165eaa1f3217714dfda75f0c72c939aadf90f0
MatheusSilva09 <matheuslucas29@gmail.com>
2026-04-15 17:43:11 -0300

    atualização

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Vendedor, Loja, Cliente, Categoria, Produto, Pedido
 
-# Register your models here.
+# Registrando as tabelas para aparecerem no painel
+admin.site.register(Vendedor)
+admin.site.register(Loja)
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Produto)
+admin.site.register(Pedido)
